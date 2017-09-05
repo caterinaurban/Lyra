5dcc1a416891279e8e24f2b05ee8fd28587bed75
Caterina Urban <caterina.urban@gmail.com>
2017-09-05 09:28:01 +0200

    interval analysis

--- tests/playground.py
+++ tests/playground.py
@@ -1,9 +1,12 @@
 from engine.liveness.liveness_analysis import LivenessAnalysis
+from engine.numerical.interval_analysis import IntervalAnalysis
 from engine.traces.traces_analysis import BoolTracesAnalysis
 from engine.usage.usage_analysis import UsageAnalysis
 
-LivenessAnalysis().main("liveness/example.py")
+#LivenessAnalysis().main("liveness/example.py")
 
-BoolTracesAnalysis().main("traces/example.py")
+#BoolTracesAnalysis().main("traces/example.py")
 
-UsageAnalysis().main("usage/example.py")
+#UsageAnalysis().main("usage/example.py")
+
+IntervalAnalysis().main("numerical/example.py")
