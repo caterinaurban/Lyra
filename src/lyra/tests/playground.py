8bfb158459a40ab9ba5f5442cc7933178bd3217f
Unknown <smadelin@student.ethz.ch>
2017-11-21 14:01:04 +0100

    Removed not needed files, moved backwards tests

--- src/lyra/tests/playground.py
+++ src/lyra/tests/playground.py
@@ -1,7 +1,7 @@
 from lyra.engine.liveness.liveness_analysis import LivenessAnalysis, StrongLivenessAnalysis
 from lyra.engine.numerical.interval_analysis import ForwardIntervalAnalysis
+from lyra.engine.numerical.interval_analysis import BackwardIntervalAnalysis
 from lyra.engine.usage.usage_analysis import UsageAnalysis
-from lyra.engine.numerical.backward_interval_analysis import BackwardIntervalAnalysis
 
 # LivenessAnalysis().main("liveness/example.py")
 
@@ -10,5 +10,3 @@ from lyra.engine.numerical.backward_interval_analysis import BackwardIntervalAna
 # UsageAnalysis().main("usage/example.py")
 
 ForwardIntervalAnalysis().main("numerical/example.py")
-
-#BackwardIntervalAnalysis().main("numerical/example_backwards.py")
--- src/lyra/unittests/interval_backwards_tests.py
+++ src/lyra/unittests/interval_backwards_tests.py
@@ -1,54 +1,32 @@
 import glob
+import os
+import unittest
 
-from lyra.abstract_domains.numerical.interval_domain import IntervalDomain, IntervalLattice, Interval
-from lyra.core.expressions import VariableIdentifier
 from lyra.engine.backward import BackwardInterpreter
 from lyra.semantics.backward import DefaultBackwardSemantics
-from lyra.unittests.generic_tests import ResultCommentsFileTestCase
-import unittest
-import ast
-import os
-import logging
 
-logging.basicConfig(level=logging.INFO, filename='unittests.log', filemode='w')
+from lyra.abstract_domains.numerical.interval_domain import IntervalState
+from lyra.unittests.runner import TestRunner
 
 
-class IntervalBackwardTestCase(ResultCommentsFileTestCase):
-    def __init__(self, source_path):
-        super().__init__(source_path)
-        self._source_path = source_path
+class IntervalTest(TestRunner):
 
-    def runTest(self):
-        logging.info(self)
-        self.render_cfg()
+    def interpreter(self):
+        return BackwardInterpreter(self.cfg, DefaultBackwardSemantics(), 3)
 
-        # find all variables
-        variable_names = sorted(
-            {node.id for node in ast.walk(self.ast_root) if
-             isinstance(node, ast.Name) and isinstance(node.ctx, ast.Store)})
-        variables = []
-        for name in variable_names:
-            variables.append(VariableIdentifier(int, name))
-
-        # init all variables to bottom
-        domain = IntervalDomain(variables)
-
-        # Run Interval numerical Analysis
-        backward_interpreter = BackwardInterpreter(self.cfg, DefaultBackwardSemantics(), 3)
-        result = backward_interpreter.analyze(domain)
-        self.render_result_cfg(result)
-        self.check_result_comments(result)
+    def state(self):
+        return IntervalState(self.variables)
 
 
-def suite():
-    s = unittest.TestSuite()
-    g = os.getcwd() + '/interval/backwards/**.py'
-    for path in glob.iglob(g):
+def test_suite():
+    suite = unittest.TestSuite()
+    name = os.getcwd() + '/interval_backwards/**.py'
+    for path in glob.iglob(name):
         if os.path.basename(path) != "__init__.py":
-            s.addTest(IntervalBackwardTestCase(path))
-    runner = unittest.TextTestRunner()
-    runner.run(s)
+            suite.addTest(IntervalTest(path))
+    return suite
 
 
 if __name__ == '__main__':
-    suite()
+    runner = unittest.TextTestRunner()
+    runner.run(test_suite())
