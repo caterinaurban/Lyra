5bf9278ab7b5f865c0a73680471f7da023bf8046
Unknown <smadelin@student.ethz.ch>
2017-12-07 14:38:04 +0100

    added tests

--- src/lyra/unittests/quality/input.py
+++ src/lyra/unittests/quality/input.py
@@ -1,11 +1,15 @@
 a: int = int(input())
+# STATE a -> (Int, [-inf, inf]), b -> (Any, [-inf, inf]), c -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf]), (Float, [-inf, inf]), (Any, [-inf, inf]), (Float, [-inf, inf])]
 if a > 10:
+    # STATE a -> (Any, [-inf, inf]), b -> (Any, [-inf, inf]), c -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf]), (Float, [-inf, inf]), (Any, [-inf, inf]), (Int, [-inf, inf])]
     b: int = int(input())
 else:
+    # STATE a -> (Any, [-inf, inf]), b -> (Any, [-inf, inf]), c -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf]), (Float, [-inf, inf]), (Any, [-inf, inf]), (Float, [-inf, inf])]
     b: float = float(input())
+# STATE a -> (Any, [-inf, inf]), b -> (Any, [-inf, inf]), c -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf]), (Float, [-inf, inf]), (Any, [-inf, inf])]
 a: str = input()
-b: float = float(input())
-while b < 10:
-    a: int = int(input())
-    b: float = b + 1
+# STATE a -> (Any, [-inf, inf]), b -> (Any, [-inf, inf]), c -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf]), (Float, [-inf, inf])]
+c: float = float(input())
+# STATE a -> (Any, [-inf, inf]), b -> (Any, [-inf, inf]), c -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf])]
 a: int = int(input())
+# FINAL a -> (Any, [-inf, inf]), b -> (Any, [-inf, inf]), c -> (Any, [-inf, inf]), .IN -> []
---
+++ src/lyra/unittests/quality/substitute.py
@@ -0,0 +1,16 @@
+
+# STATE a -> (Any, [-inf, inf]), b -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf]), (Int, [-inf, 9])]
+a: int = int(input())
+# STATE a -> (Int, [-inf, 9]), b -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf])]
+b: int = int(input())
+# STATE a -> (Int, [-inf, 9]), b -> (Any, [-inf, inf]), .IN -> []
+b: int = a
+# STATE a -> (Any, [-inf, inf]), b -> (Int, [-inf, 9]), .IN -> []
+b: int = b + 1
+# STATE a -> (Any, [-inf, inf]), b -> (Int, [-inf, 10]), .IN -> []
+a: int = b
+
+# STATE a -> (Int, [-inf, 10]), b -> (Any, [-inf, inf]), .IN -> []
+if a > 10:
+    # STATE a -> (⊥, ⊥), b -> (⊥, ⊥), .IN -> ⊥
+    raise ValueError
