37f86ac49592919265f724958fd4d78c915a0e57
Unknown <smadelin@student.ethz.ch>
2017-12-14 11:07:13 +0100

    input collector with loop support

--- src/lyra/quality_analysis/static_analyzer/input_assumption_collector.py
+++ src/lyra/quality_analysis/static_analyzer/input_assumption_collector.py
@@ -1,10 +1,10 @@
 from copy import deepcopy
 
-from lyra.abstract_domains.quality.assumption_lattice import AssumptionLattice
 from lyra.core.cfg import ControlFlowGraph, Loop, Edge
 from collections import deque
 
 from lyra.core.statements import Call, Assignment, VariableAccess, LiteralEvaluation
+from lyra.core.types import IntegerLyraType
 
 
 class InputAssumptionCollector:
@@ -22,8 +22,6 @@ class InputAssumptionCollector:
         Iterates through the control flow graph and gathers information about the order
         the inputs are read and combines it with the assumptions
 
-        :param cfg: Controlflowgraph
-        :param input_assumptions: Assumption of inputs
         :return: Assumptions about inputs in correct order
         """
 
@@ -38,13 +36,16 @@ class InputAssumptionCollector:
 
             # check if current_node already analyzed
             if current_node not in self.nodes_done:
-
-                # if loop_in do special separate analysis
                 if isinstance(current_node, Loop):
-                    self.analyze_loop(current_node, current_node, dict())
-
-                if not self.analyze_node(current_node, self.node_to_input):
-                    break
+                    input_assmps = self.analyze_loop(current_node)
+                    information_loss = not self.get_prev_assmp(current_node, self.node_to_input)
+                    if information_loss:
+                        break
+                    self.node_to_input[current_node].extend(input_assmps)
+                else:
+                    information_loss = not self.analyze_node(current_node, self.node_to_input)
+                    if information_loss:
+                        break
 
             self.nodes_done.append(current_node)
 
@@ -53,25 +54,27 @@ class InputAssumptionCollector:
 
             next_edge = [e for e in self.edges_todo if e[0] == current_node][0]
             current_node = next_edge[1]
-            # move edge from to do to done
+
             self.edges_todo.remove(next_edge)
             self.edges_done.append(next_edge)
-            # y of (x,y) is now current_node
 
         return self.node_to_input[current_node]
 
-    def analyze_node(self, current_node, node_to_input):
-        # if multiple edges in done with (x, current_node)
-            # merge, check if still enough information
-        sources = [e[0] for e in self.edges_done if e[1] == current_node]
+    def get_prev_assmp(self, current_node, node_to_input):
+        sources = [e[0] for e in self.edges_done if e[1] == current_node
+                   and self.cfg.edges[e].kind != Edge.Kind.LOOP_OUT]
         if len(sources) == 1:
-            source = sources[0]
-            if source in node_to_input:
-                node_to_input[current_node] = deepcopy(node_to_input[source])
+            if sources[0] in node_to_input:
+                node_to_input[current_node] = deepcopy(node_to_input[sources[0]])
         elif len(sources) > 1:
             if not self.handle_multiple_sources(sources, current_node, node_to_input):
                 return False
+        return True
 
+    def analyze_node(self, current_node, node_to_input):
+        if len(node_to_input.keys()) > 0:
+            if not self.get_prev_assmp(current_node, node_to_input):
+                return False
         # analyze statements
         for stmt in current_node.stmts:
             self.analyze_stmt_for_input(stmt, current_node, node_to_input)
@@ -83,7 +86,7 @@ class InputAssumptionCollector:
         for assmp in [node_to_input[source] for source in sources[1:]]:
             if len(assmp) != len(merged_assmps):
                 node_to_input[current_node] = []
-                merged_assmps = self.find_common_assmp(sources, self.edges_done)
+                merged_assmps = self.find_common_assmp(sources, self.edges_done, node_to_input)
                 merge_successful = False
             for assmp1, assmp2 in zip(merged_assmps, assmp):
                 assmp1.join(assmp2)
@@ -93,7 +96,6 @@ class InputAssumptionCollector:
     def find_common_assmp(self, nodes, edges, node_to_input):
         node_sources = [[n] for n in nodes]
         edges_todo = deepcopy(edges)
-        #nodes_done = deepcopy(nodes)
         while len(edges_todo) > 0:
             for edge in edges_todo:
                 for node_source in node_sources:
@@ -125,68 +127,61 @@ class InputAssumptionCollector:
             else:
                 raise NotImplementedError(f"Analysis for {type(curr_stmt)} not yet implemented")
 
-    def analyze_loop(self, current_node, loop_in_node, node_to_input):
-        # check if there are edges with (x, current_node) that have to be handled first
-        edges_with_target = [e for e in self.edges_todo if e[1] == current_node
-                             and self.cfg.edges[e].kind != Edge.Kind.LOOP_OUT]
-        if len(edges_with_target) > 0:
-            current_node = edges_with_target[0][0]
-            self.analyze_loop(current_node, loop_in_node, node_to_input)
-
-        # check if current_node already analyzed
-        if current_node not in self.nodes_done:
-            self.analyze_node(current_node, node_to_input)
-
-        self.nodes_done.append(current_node)
-
-        next_edge = [e for e in self.edges_todo if e[0] == current_node][0]
-        current_node = next_edge[1]
-
-        self.edges_todo.remove(next_edge)
-        self.edges_done.append(next_edge)
-
-        if current_node == loop_in_node:
-            # TODO put multiple of assumptions from local node_to_input into self.node_to_input
-            return
-
-        self.analyze_loop(current_node, loop_in_node, node_to_input)
-
-
-
-    def analyze_loop_old(self, edges_todo, edges_done, current_node, loop_in_node, assumption, node_to_input, node_to_input_loop, iter_var, curr_iter, loop_cond, input_assumptions):
-        """"""
-        if current_node != loop_in_node:
-            # check if there are edges with (x, current_node)
-            edges_with_target = [e for e in edges_todo if e[1] == current_node]
+    def analyze_loop(self, current_node):
+        loop_node_to_input = dict()
+        loop_in_node = current_node
+        one_iter_done = False
+        while current_node != loop_in_node or not one_iter_done:
+            one_iter_done = True
+            # check if there are edges with (x, current_node) that have to be handled first
+            edges_with_target = [e for e in self.edges_todo if e[1] == current_node
+                                 and self.cfg.edges[e].kind != Edge.Kind.LOOP_OUT]
             if len(edges_with_target) > 0:
-                self.analyze_loop(edges_todo, edges_done, edges_with_target[0][0], loop_in_node, assumption, node_to_input, node_to_input_loop, iter_var, curr_iter, loop_cond, input_assumptions)
+                current_node = edges_with_target[0][0]
+                self.analyze_loop(current_node)
 
+            # check if current_node already analyzed
+            if current_node not in self.nodes_done:
+                self.analyze_node(current_node, loop_node_to_input)
 
-        # if multiple edges in done with (x, current_node)
-            # merge, check if still enough information
-        sources = [e[0] for e in edges_done if e[1] == current_node]
-        if len(sources) == 1:
-            source = sources[0]
-            if source in node_to_input:
-                node_to_input[current_node] = deepcopy(node_to_input[source])
-        elif len(sources) > 1:
-            self.handle_multiple_sources(self, node_to_input, sources, edges_done, current_node)
+            self.nodes_done.append(current_node)
 
-        # analyze statements
-        for stmt in current_node.stmts:
-            self.analyze_stmt_for_input(stmt, current_node, node_to_input, input_assumptions)
+            next_edges = [e for e in self.edges_todo if e[0] == current_node]
+            if current_node == loop_in_node:
+                next_edge = [e for e in next_edges if self.cfg.edges[e].kind == Edge.Kind.LOOP_IN][0]
+            else:
+                next_edge = next_edges[0]
+            current_node = next_edge[1]
 
+            self.edges_todo.remove(next_edge)
+            self.edges_done.append(next_edge)
 
-        next_edges = [e for e in edges_todo if e[0] == current_node]
+        condition_edge = [e for e in self.edges_done if e[0] == current_node
+                                     and self.cfg.edges[e].kind == Edge.Kind.LOOP_IN][0]
+        condition = self.cfg.edges[condition_edge].condition
+        num_iter = self.find_number_of_iterations(condition)
+        return loop_node_to_input[next_edge[0]] * num_iter
 
-        if len(next_edges) == 0:
-            return
-        next_edge = next_edges[0]
-        current_node = next_edge[1]
-        edges_todo.remove(next_edge)
-        edges_done.append(next_edge)
-
-        if current_node == loop_in_node:
-            return
-
-        self.analyze_loop(edges_todo, edges_done, current_node, loop_in_node, assumption, node_to_input, node_to_input_loop, iter_var, curr_iter, loop_cond, input_assumptions)
+    def find_number_of_iterations(self, condition):
+        if not (isinstance(condition, Call) and condition.name == 'in'):
+            raise NotImplementedError("Loop analysis only implemented for \'in\' condition.")
+        in_argument = condition.arguments[1]
+        if isinstance(in_argument, Call) and in_argument.name == 'range':
+            if len(in_argument.arguments) == 1:
+                num_range = in_argument.arguments[0]
+                if isinstance(num_range, LiteralEvaluation):
+                    num_range_literal = num_range.literal
+                    if isinstance(num_range_literal.typ, IntegerLyraType):
+                        return int(num_range_literal.val)
+                    else:
+                        error = f"Loop analysis not implemented for range call with argument type {num_range_literal.typ}."
+                        raise NotImplementedError(error)
+                else:
+                    error = f"Loop analysis not implemented for range call with argument {num_range}."
+                    raise NotImplementedError(error)
+            else:
+                error = f"Loop analysis not implemented for range call with {len(in_argument.arguments)} arguments"
+                raise NotImplementedError(error)
+        else:
+            error = f"Loop analysis not implemented for condition {condition}"
+            raise NotImplementedError(error)
