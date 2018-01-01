6939987e51bd5f46a9c93182ea9c66183fe045ff
Unknown <smadelin@student.ethz.ch>
2018-01-01 12:06:36 +0100

    unittest

--- src/lyra/unittests/quality/analysis/if_else_infoloss.py
+++ src/lyra/unittests/quality/analysis/if_else_infoloss.py
@@ -1,4 +1,4 @@
-# STATE i -> (Any, [-inf, inf]), a -> (Any, [-inf, inf]), b -> (Any, [-inf, inf]), c -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf]), (Int, [-inf, inf])]
+# STATE i -> (Any, [-inf, inf]), a -> (Any, [-inf, inf]), b -> (Any, [-inf, inf]), c -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf]), (Int, [-inf, inf]), (Float, [-inf, inf])]
 i: int = 0
 a: int = int(input())
 if a > 10:
@@ -6,5 +6,5 @@ if a > 10:
 else:
     c: int = int(input())
     c: int = int(input())
-
+b: int = float(input())
 
