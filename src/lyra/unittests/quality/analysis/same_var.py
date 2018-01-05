ca3d510e313275bd7001b085553c6a6852d71975
Madelin Schumacher <smadelin@student.ethz.ch>
2018-01-05 12:40:01 +0100

    removed tests

--- src/lyra/unittests/quality/analysis/same_var.py
+++
@@ -1,8 +0,0 @@
-# INITIAL a -> (Any, [-inf, inf]), .IN -> [(Any, [-inf, inf]), (Int, [-inf, inf]), (Int, [-inf, inf])]
-a: str = input()
-# STATE a -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf]), (Int, [-inf, inf])]
-a: int = int(input())
-# STATE a -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf])]
-a: str = input()
-a: int = int(a)
-# FINAL a -> (Any, [-inf, inf]), .IN -> []
--- src/lyra/unittests/quality/analysis/type.py
+++
@@ -1,9 +0,0 @@
-in1: float = input()
-in2: float = input()
-in3: float = input()
-
-if in1 > 10:
-    N: int = in2
-else:
-    N: float = in3
-N: float = N + 1
