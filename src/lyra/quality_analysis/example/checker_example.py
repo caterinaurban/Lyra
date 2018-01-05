79de5ce1cd2f8b53d81ce239cbc233239faab585
Unknown <smadelin@student.ethz.ch>
2018-01-05 10:40:22 +0100

    input checker executes program, example

--- src/lyra/quality_analysis/example/checker_example.py
+++ src/lyra/quality_analysis/example/checker_example.py
@@ -1,14 +1,8 @@
 a: int = int(input())
 i: int = 0
-if a > 10:
-    b: int = int(input())
-    b: int = int(input())
-    b: int = int(input())
-    b: int = int(input())
-else:
-    b: int = int(input())
-    b: int = int(input())
-b: int = float(input())
-b: int = int(input())
-b: int = int(input())
-print(b)
+for i in range(10):
+    b: float = float(input())
+    if b > 10:
+        print(b)
+    else:
+        print(1)
---
+++ src/lyra/quality_analysis/example/execute_program.sh
@@ -0,0 +1,14 @@
+python3.6 example/checker_example.py <<EOF
+3
+5
+3
+3
+5
+3
+3
+3
+3
+3
+3
+
+EOF
