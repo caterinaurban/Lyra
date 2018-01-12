9d0b30b1b4b139745a13d7132c8c130e1884805e
Madelin Schumacher <smadelin@student.ethz.ch>
2018-01-12 09:16:48 +0100

    unittest

--- src/lyra/unittests/quality/analysis/multiple_for_range.py
+++ src/lyra/unittests/quality/analysis/multiple_for_range.py
@@ -4,6 +4,6 @@ for i in range(1, 10, 4):
     c: int = float(input())
     for i in range(1, 5):
         c: int = int(input())
-    for i in range(2):
+    for i in range(10, 5, -3):
         c: int = float(input())
 e: int = int(input())
