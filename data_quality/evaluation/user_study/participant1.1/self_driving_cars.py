511d0680aafe12c2370a491df61b6fdb588a75c6
Madelin Schumacher <smadelin@student.ethz.ch>
2018-03-03 08:12:11 +0100

    participant 1

---
+++ data_quality/evaluation/user_study/participant1.1/self_driving_cars.py
@@ -0,0 +1,20 @@
+cases: int = int(input())
+for cc in range(cases):
+    distance: float = float(input())
+    n: int = int(input())
+    max_time: float = 0
+    for i in range(n):
+        position: int = int(input())
+        if distance < position:
+            raise ValueError("The distance cannot be smaller than the current position.")
+        speed: float = float(input())
+        if speed < 0:
+            raise ValueError("The speed cannot be negative.")
+        val: float = (distance - position) / speed
+        if val > max_time:
+            max_time: int = val
+    print('Trip #')
+    print(cc+1)
+    print("with speed")
+    print(distance / max_time)
+    print("")
