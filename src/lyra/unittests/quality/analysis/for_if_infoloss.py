4b563becf78017164d684abc0b3979fc8d78887e
Caterina Urban <caterina.urban@gmail.com>
2018-06-15 13:26:56 +0200

    cleanup

--- src/lyra/unittests/quality/analysis/for_if_infoloss.py
+++
@@ -1,9 +0,0 @@
-# INITIAL a -> (Any, [-inf, inf]), i -> (Int, [-inf, inf]), b -> (Any, [-inf, inf]), c -> (Any, [-inf, inf]), .IN -> [(Int, [-inf, inf])]
-a: int = int(input())
-for i in range(20):
-    b: int = int(input())
-    if b > 10:
-        c: int = float(input())
-
-
-
