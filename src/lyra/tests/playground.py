f7bd9a21915d8796fb0c7929fa80ec619b792301
Caterina Urban <caterina.urban@gmail.com>
2018-06-15 13:29:13 +0200

    cleanup

--- src/lyra/tests/playground.py
+++ src/lyra/tests/playground.py
@@ -2,7 +2,6 @@ from lyra.engine.assumption.assumption_analysis import TypeAnalysis
 from lyra.engine.liveness.liveness_analysis import LivenessAnalysis, StrongLivenessAnalysis
 from lyra.engine.numerical.interval_analysis import ForwardIntervalAnalysis
 from lyra.engine.numerical.interval_analysis import BackwardIntervalAnalysis
-from lyra.engine.quality.assumption_analysis import AssumptionAnalysis
 from lyra.engine.usage.usage_analysis import SimpleUsageAnalysis
 
 # LivenessAnalysis().main("liveness/example.py")
@@ -11,12 +10,8 @@ from lyra.engine.usage.usage_analysis import SimpleUsageAnalysis
 
 # SimpleUsageAnalysis().main("usage/example.py")
 
-# BoolTracesAnalysis().main("traces/example.py")
-
-# BackwardIntervalAnalysis().main("quality/example.py")
-
 # ForwardIntervalAnalysis().main("numerical/example.py")
 
-# AssumptionAnalysis().main("quality/example.py")
+# BackwardIntervalAnalysis().main("quality/example.py")
 
 TypeAnalysis().main("assumption/example.py")
