314b871cfd400ce4617718502b7b27422c16cb36
ELowis <game.playerle@ymail.com>
2018-07-10 15:36:45 +0200

    example

--- src/lyra/tests/numerical/dict_example.py
+++ src/lyra/tests/numerical/dict_example.py
@@ -1,7 +1,13 @@
 v: int = 0
+i: int = input()
 d: Dict[int,int] = {3:2}
 d[4]: int = 1
 d[v]: int = 7
 d[1]: int = v
 v2: int = d[3]
 d[2]: int = 3 + v2
+k: int = input()
+if k in d.keys():
+   v2: int = d[1]
+# weak update
+d[i]: int = -4
--- src/lyra/tests/playground.py
+++ src/lyra/tests/playground.py
@@ -1,4 +1,5 @@
 from lyra.engine.assumption.assumption_analysis import TypeAnalysis
+from lyra.engine.data_structures.dict_content_analysis import DictContentAnalysis
 from lyra.engine.liveness.liveness_analysis import LivenessAnalysis, StrongLivenessAnalysis
 from lyra.engine.numerical.interval_analysis import ForwardIntervalAnalysis
 from lyra.engine.numerical.interval_analysis import BackwardIntervalAnalysis
@@ -6,12 +7,14 @@ from lyra.engine.usage.usage_analysis import SimpleUsageAnalysis
 
 # LivenessAnalysis().main("liveness/example.py")
 
-# StrongLivenessAnalysis().main("liveness/example.py")
+# StrongLivenessAnalysis().main("dictionary/dict_descr_example.py")
 
-# SimpleUsageAnalysis().main("usage/example.py")
+# SimpleUsageAnalysis().main("dictionary/BRCA_example.py")
 
 # ForwardIntervalAnalysis().main("numerical/example.py")
 
 # BackwardIntervalAnalysis().main("quality/example.py")
 
-TypeAnalysis().main("assumption/example.py")
+# TypeAnalysis().main("assumption/example.py")
+
+DictContentAnalysis().main("numerical/dict_example.py")
