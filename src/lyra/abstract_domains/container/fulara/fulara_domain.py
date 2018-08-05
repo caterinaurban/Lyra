50282ee854a42f0c05a840f74d2853e0190d15ee
ELowis <game.playerle@ymail.com>
2018-08-05 18:30:14 +0200

    bug fix: assign/assume for bottom

--- src/lyra/abstract_domains/container/fulara/fulara_domain.py
+++ src/lyra/abstract_domains/container/fulara/fulara_domain.py
@@ -669,6 +669,9 @@ class FularaState(State):
 
     @copy_docstring(State._assign)
     def _assign(self, left: Expression, right: Expression):
+        if self.is_bottom():      # unreachable
+            return self
+
         all_ids = left.ids().union(right.ids())
 
         if all(type(id.typ) in scalar_types for id in all_ids):   # TODO: use not any?
@@ -771,6 +774,9 @@ class FularaState(State):
 
     @copy_docstring(State._assume)
     def _assume(self, condition: Expression) -> 'FularaState':
+        if self.is_bottom():      # unreachable
+            return self
+
         condition = NegationFreeNormalExpression().visit(condition)     # eliminate negations
 
         if isinstance(condition, BinaryComparisonOperation):
