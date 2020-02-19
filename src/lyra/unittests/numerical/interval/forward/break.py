ffd01da04887dd534afc65152c70d6a0132d29e9
Caterina Urban <caterina.urban@gmail.com>
2020-02-19 15:48:49 +0100

    revert unittests

--- src/lyra/unittests/numerical/interval/forward/break.py
+++ src/lyra/unittests/numerical/interval/forward/break.py
@@ -1,32 +1,20 @@
 
 for x in range(10):
-    # STATE:  x -> [0, 9], y -> [-inf, inf]
+    # STATE:  x -> [0, 9]
     x = x # dummy assignment
     if x > 7:
-        # STATE: x -> [8, 9], y -> [-inf, inf]
+        # STATE: x -> [8, 9]
         x: int = x + 2
-        # STATE: x -> [10, 11], y -> [-inf, inf]
+        # STATE: x -> [10, 11]
     else:
-        # STATE: x -> [0, 7], y -> [-inf, inf]
+        # STATE: x -> [0, 7]
         x: int = x + 1
-        # STATE: x -> [1, 8], y -> [-inf, inf]
+        # STATE: x -> [1, 8]
         if x == 8:
-            # STATE: x -> [8, 8], y -> [-inf, inf]
+            # STATE: x -> [8, 8]
             x = x       # dummy assignment
             break
-        # STATE: x -> [1, 7], y -> [-inf, inf]
+        # STATE: x -> [1, 7]
         x = x  # dummy assignment
-        # STATE: x -> [1, 7], y -> [-inf, inf]
-    # STATE: x -> [1, 11], y -> [-inf, inf]
-    for y in range(20):
-        # STATE: x -> [1, 11], y -> [0, 19]
-        if y > x:
-            # STATE: x -> [1, 11], y -> [2, 19]
-            break
-        # STATE: x -> [1, 11], y -> [0, 11]
-        if y > 10:
-            # STATE: x -> [1, 11], y -> [11, 11]
-            break
-        # STATE: x -> [1, 11], y -> [0, 10]
     x = x   # dummy assignment
-    # STATE: x -> [1, 11], y -> [-inf, inf]
+    # STATE: x -> [1, 11]
--- src/lyra/unittests/numerical/interval/forward/max.py
+++ src/lyra/unittests/numerical/interval/forward/max.py
@@ -15,8 +15,3 @@ a: int = 10
 b: int = 20
 c: int = max(a, b) if a > 0 and b > 0 else min(a, b)
 # STATE: a -> [10, 10], b -> [20, 20], c -> [20, 20]
-
-x: int = 0
-for i in range(max(a, b)):
-    x += 1
-print(x)
