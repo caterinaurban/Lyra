802a148f2f080b06ed16b34b00b5449b16a9820c
Caterina <caterina.urban@gmail.com>
2023-02-15 21:19:55 +0100

    adding not-implemented exceptions

--- src/lyra/abstract_domains/usage/dataframe_usage_domain.py
+++ src/lyra/abstract_domains/usage/dataframe_usage_domain.py
@@ -31,77 +31,77 @@ class DataFrameColumnUsageState(BoundedLattice, State):
         items = sorted(self.store.items(), key=lambda x: x[0].name)
         return "; ".join("{} -> {}".format(variable, do(value)) for variable, value in items)
 
-    def _less_equal(self, other: 'Lattice') -> bool:
-        return self
+    def _less_equal(self, other: 'DataFrameColumnUsageState') -> bool:
+        raise NotImplementedError('_less_equal in DataFrameColumnUsageState is not yet implemented!')
 
     def _join(self, other: 'DataFrameColumnUsageState') -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('_join in DataFrameColumnUsageState is not yet implemented!')
 
     def _meet(self, other: 'DataFrameColumnUsageState') -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('_meet in DataFrameColumnUsageState is not yet implemented!')
 
     def _widening(self, other: 'DataFrameColumnUsageState') -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('_widening in DataFrameColumnUsageState is not yet implemented!')
 
     def _assign_variable(self, left: VariableIdentifier, right: Expression) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('_assign_variable in DataFrameColumnUsageState is not yet implemented!')
 
     def _assign_subscription(self, left: Subscription, right: Expression) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('_assign_subscription in DataFrameColumnUsageState is not yet implemented!')
 
     def _assign_slicing(self, left: Slicing, right: Expression) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('_assign_slicing in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_variable(self, condition: VariableIdentifier, neg: bool = False) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('_assume_variable in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_subscription(self, condition: Subscription, neg: bool = False) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('_assume_subscription in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_eq_comparison(self, condition: BinaryComparisonOperation, bwd: bool = False):
-        return self
+        raise NotImplementedError('_assume_eq_comparison in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_noteq_comparison(self, condition: BinaryComparisonOperation, bwd: bool = False):
-        return self
+        raise NotImplementedError('_assume_noteq_comparison in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_lt_comparison(self, condition: BinaryComparisonOperation, bwd: bool = False):
-        return self
+        raise NotImplementedError('_assume_lt_comparison in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_lte_comparison(self, condition: BinaryComparisonOperation, bwd: bool = False):
-        return self
+        raise NotImplementedError('_assume_lte_comparison in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_gt_comparison(self, condition: BinaryComparisonOperation, bwd: bool = False):
-        return self
+        raise NotImplementedError('_assume_gt_comparison in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_gte_comparison(self, condition: BinaryComparisonOperation, bwd: bool = False):
-        return self
+        raise NotImplementedError('_assume_gte_comparison in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_is_comparison(self, condition: BinaryComparisonOperation, bwd: bool = False):
-        return self
+        raise NotImplementedError('_assume_is_comparison in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_isnot_comparison(self, condition: BinaryComparisonOperation, bwd: bool = False):
-        return self
+        raise NotImplementedError('_assume_isnot_comparison in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_in_comparison(self, condition: BinaryComparisonOperation, bwd: bool = False):
-        return self
+        raise NotImplementedError('_assume_in_comparison in DataFrameColumnUsageState is not yet implemented!')
 
     def _assume_notin_comparison(self, condition: BinaryComparisonOperation, bwd: bool = False):
-        return self
+        raise NotImplementedError('_assume_notin_comparison in DataFrameColumnUsageState is not yet implemented!')
 
     def enter_if(self) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('enter_if in DataFrameColumnUsageState is not yet implemented!')
 
     def exit_if(self) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('exit_if in DataFrameColumnUsageState is not yet implemented!')
 
     def enter_loop(self) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('enter_loop in DataFrameColumnUsageState is not yet implemented!')
 
     def exit_loop(self) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('exit_loop in DataFrameColumnUsageState is not yet implemented!')
 
     def forget_variable(self, variable: VariableIdentifier) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('forget_variable in DataFrameColumnUsageState is not yet implemented!')
 
     def _output(self, output: Expression) -> 'DataFrameColumnUsageState':
         if isinstance(output, VariableIdentifier):
@@ -109,13 +109,18 @@ class DataFrameColumnUsageState(BoundedLattice, State):
         return self
 
     def _substitute_variable(self, left: VariableIdentifier, right: Expression) -> 'DataFrameColumnUsageState':
+        used = any(usage.is_top() for usage in self.store[left].values())
+        scoped = any(usage.is_scoped() for usage in self.store[left].values())
+        if used or scoped:
+            # the assigned variable is used or scoped
+            self.store[left] = {None: UsageLattice().written()}
         return self
 
     def _substitute_subscription(self, left: Subscription, right: Expression) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('_substitute_subscription in DataFrameColumnUsageState is not yet implemented!')
 
     def _substitute_slicing(self, left: Slicing, right: Expression) -> 'DataFrameColumnUsageState':
-        return self
+        raise NotImplementedError('_substitute_slicing in DataFrameColumnUsageState is not yet implemented!')
 
     def _drop_dataframe_column(self, dataframe: Expression, columns: Expression):
         if isinstance(dataframe, VariableIdentifier):
