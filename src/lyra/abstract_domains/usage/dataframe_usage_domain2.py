277aa03ac1fd640d05c97df35095d8120ff60b59
Pierre Goutagny <pierre.goutagny@ens-lyon.fr>
2024-04-11 15:40:53 +0200

    Add _output for variable and _substitute_variable for dataframes

--- src/lyra/abstract_domains/usage/dataframe_usage_domain2.py
+++ src/lyra/abstract_domains/usage/dataframe_usage_domain2.py
@@ -10,10 +10,11 @@ from lyra.abstract_domains.usage.usage_lattice import UsageLattice
 from lyra.abstract_domains.usage.usage_domain import UsageStore
 from lyra.core.expressions import Slicing, Expression, Subscription, \
     VariableIdentifier, BinaryComparisonOperation, Literal, ListDisplay
-from lyra.core.types import LyraType, DataFrameLyraType
+from lyra.core.types import LyraType, DataFrameLyraType, StringLyraType
 from lyra.core.utils import copy_docstring
 
 ColumnName = Union[str, None]
+
 
 class DataFrameColumnType(LyraType):
     """Dummy type for names of columns of dataframes.
@@ -33,11 +34,41 @@ class DataFrameColumnIdentifier(VariableIdentifier):
                      represents any column)
         """
 
-        # TODO find out what "special" does and see if it applies here
-        super().__init__(typ=DataFrameColumnType, name=name)
+        if isinstance(name, str) or name is None:
+            super().__init__(typ=DataFrameColumnType, name=name)
+        elif isinstance(name, Literal) and name.typ == StringLyraType():
+            super().__init__(typ=DataFrameColumnType, name=name.val)
+        elif isinstance(name, DataFrameColumnIdentifier):
+            return
+        else:
+            raise ValueError("Cannot create DataFrameColumnIdentifier out of "
+                             f"{name} of type {type(name)}")
+
+    # used when sorting column names
+    def __lt__(self, other):
+        return other.name is None or str(self) < str(other)
 
     def __str__(self):
-        return "_" if self.name is None else super().__str__(self)
+        return "_" if self.name is None else super().__str__()
+
+def _get_columns(df: VariableIdentifier, expr: Expression) -> Set['DataFrameColumnIdentifier']:
+    """Return the set of the columns of [df] present in [expr]"""
+    columns = set()
+
+    for e in walk(expr):
+        if isinstance(e, Subscription) and isinstance(e.typ, DataFrameLyraType):
+            if not e.target == df:
+                continue
+            try:
+                columns.add(DataFrameColumnIdentifier(e.key))
+            except ValueError:
+                # if the column name cannot be transformed into a column
+                # identifier, then act as if there were no columns so the next
+                # action applies to the wole dataframe
+                return {}
+
+    return columns
+
 
 class DataFrameColumnUsageLattice(UsageStore):
     """A store mapping each column of a dataframe to its usage status.
@@ -57,7 +88,56 @@ class DataFrameColumnUsageLattice(UsageStore):
     # FIXME methods such as add_variable must create DataFrameColumnIdentifiers
 
     def __repr__(self):
-        return "{" + super().__repr__() + "}"
+        items = sorted(self.store.items())
+        return "{" \
+                + ", ".join("{} -> {}".format(variable, value) for variable, value in items) \
+                + "}"
+
+    def top(self, columns={}):
+        """Use some columns of a dataframe.
+
+        If no column is provided, then the whole dataframe is used.
+
+        :param columns: Set of columns to overwrite.
+        """
+        if columns == {}:
+            super().top()
+            return self
+        else:
+            for col in columns:
+                if col not in self.variables:
+                    self.add_variable(col)
+                self.store[col].top()
+        return self
+
+    def _written_whole_dataframe(self):
+        """Overwrite a whole dataframe.
+        This loses the column information.
+        """
+        self.__init__()
+        self.store[DataFrameColumnIdentifier(None)].written()
+        return self
+
+    def written(self, columns={}):
+        """Overwrite some columns of a dataframe.
+
+        If no column is provided, then the whole dataframe is overwritten.
+
+        :param columns: Set of columns to overwrite.
+        """
+        if columns == {}:
+            self._written_whole_dataframe()
+            return self
+        else:
+            for col in columns:
+                if col not in self.variables:
+                    self.add_variable(col)
+                self.store[col].written()
+        return self
+
+    def is_scoped(self):
+        """The current dataframe is scoped if all of its columns are scoped"""
+        return all(element.is_scoped() for element in self.store.values())
 
 # TODO rename? this is not only about DataFrames
 class DataFrameColumnUsageState(Stack, State):
@@ -163,11 +243,35 @@ class DataFrameColumnUsageState(Stack, State):
     def forget_variable(self, variable: VariableIdentifier) -> 'DataFrameColumnUsageState':
         raise NotImplementedError('forget_variable in DataFrameColumnUsageState is not yet implemented!')
 
+    @copy_docstring(State._output)
     def _output(self, output: Expression) -> 'DataFrameColumnUsageState':
-        raise NotImplementedError('_output in DataFrameColumnUsageState is not yet implemented!')
+        if isinstance(output, Subscription):
+            raise NotImplementedError(f'_output for subscription {output} not yet implemented!')
+        else:
+            for identifier in output.ids():
+                self.lattice.store[identifier].top()
+        return self
 
     def _substitute_variable(self, left: VariableIdentifier, right: Expression) -> 'DataFrameColumnUsageState':
-        raise NotImplementedError('_substitute_variable in DataFrameColumnUsageState is not yet implemented!')
+        # expression of the form x = e
+        # only change the state if the assigned variable is U or S
+        if self.lattice.store[left].is_top() or self.lattice.store[left].is_scoped():
+            self.lattice.store[left].written()
+            # if left.is_dictionary:
+            #     self.lattice.keys[left.keys].written()
+            #     self.lattice.values[left.values].written()
+            for identifier in right.ids():
+                if isinstance(identifier.typ, DataFrameLyraType):
+                    used_columns = _get_columns(identifier, right)
+                    self.lattice.store[identifier].top(used_columns)
+                else:
+                    self.lattice.store[identifier].top()
+                # if identifier.is_dictionary:
+                #     self.lattice.keys[identifier.keys].top()
+                #     self.lattice.values[identifier.values].top()
+        return self
+
+
 
     def _substitute_subscription(self, left: Subscription, right: Expression) -> 'DataFrameColumnUsageState':
         raise NotImplementedError('_substitute_subscription in DataFrameColumnUsageState is not yet implemented!')
