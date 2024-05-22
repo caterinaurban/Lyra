8a1d101bedd7e1cf06bd1a77d7c16a8a119756e6
Pierre Goutagny <pierre.goutagny@ens-lyon.fr>
2024-05-22 09:15:24 +0200

    Don't print length analysis, and fix test expects

--- src/lyra/unittests/usage/dataframes/handcraft_example_5b.py
+++ src/lyra/unittests/usage/dataframes/handcraft_example_5b.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
-# INITIAL: df -> {_: W}; sub -> {_: W}
+# INITIAL: df -> {_ -> W}; sub -> {_ -> W}
 df: pd.DataFrame = pd.read_csv("...")
 
-# STATE: df -> {"A": U, "B": N, "X": U, "Y": U _: N}; sub -> {_: W}
+# STATE: df -> {A -> U, B -> N, X -> U, Y -> U, _ -> N}; sub -> {_ -> W}
 sub: pd.DataFrame = df[["A", "B", "Y"]]
 
-# STATE: df -> {"X": U, "Y": U, _: N}; sub -> {"B": W, _: U}
+# STATE: df -> {X -> U, Y -> U, _ -> N}; sub -> {B -> W, _ -> U}
 sub["B"] = df["X"] + df["Y"]
 
-# STATE: df -> {_: N}; sub -> {_: U}
+# STATE: df -> {_ -> N}; sub -> {_ -> U}
 sub.head()
 
-# FINAL: df -> {_: N}; sub -> {_: N}
+# FINAL: df -> {_ -> N}; sub -> {_ -> N}
